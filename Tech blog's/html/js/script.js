a147e6b8647ed4221a1f3020448a9a7d7d57bdcf
LOGESH.G <128713913+Logeshgopu@users.noreply.github.com>
2023-11-09 00:05:11 +0530

    Add files via upload

---
+++ Tech blog's/html/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("more2btn").addEventListener("click", function() {
+    window.location.href = "more2.html";
+});
+
+document.getElementById("more3btn").addEventListener("click", function() {
+    window.location.href = "more3.html";
+});
+
+document.getElementById("more1btn").addEventListener("click", function() {
+    window.location.href = "more1.html";
+});
+
+document.getElementById("more4btn").addEventListener("click", function() {
+    window.location.href = "more4.html";
+});
